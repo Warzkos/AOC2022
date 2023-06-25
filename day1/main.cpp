e6d0bed055e9226faa562a66c303609fd4e0bcf3
Hubert Droździak <hubertdrozdziak@gmail.com>
2023-06-25 16:05:40 +0200

    day1 complete

--- day1/main.cpp
+++ day1/main.cpp
@@ -7,19 +7,60 @@ int main() {
   std::string str;
   int maxSum = 0;
   int currSum = 0;
-  int i=0;
+  int Sum2=0;
+  int Sum3=0;
 
   std::ifstream File("input.txt");
   while (getline (File, str)) {
-    if(str.length()==1){
-      if(currSum>maxSum) maxSum = currSum;
+    if(str.length()==0){
+      if(currSum>maxSum) {
+        maxSum = currSum;
+      }// else if (currSum>Sum2){
+      //   Sum2=currSum;
+      // } else if (currSum>Sum3){
+      //   Sum3=currSum;
+      // }
       currSum = 0;
       continue;
     }
-
+    currSum+=stoi(str);
+  }
+  File.clear();
+  File.seekg(0);
+  while (getline (File, str)) {
+    if(str.length()==0){
+      if(currSum>Sum2 && currSum<maxSum) {
+        Sum2 = currSum;
+      }// else if (currSum>Sum2){
+      //   Sum2=currSum;
+      // } else if (currSum>Sum3){
+      //   Sum3=currSum;
+      // }
+      currSum = 0;
+      continue;
+    }
+    currSum+=stoi(str);
+  }
+  File.clear();
+  File.seekg(0);
+  while (getline (File, str)) {
+    if(str.length()==0){
+      if(currSum>Sum3 && currSum<Sum2) {
+        Sum3 = currSum;
+      }// else if (currSum>Sum2){
+      //   Sum2=currSum;
+      // } else if (currSum>Sum3){
+      //   Sum3=currSum;
+      // }
+      currSum = 0;
+      continue;
+    }
     currSum+=stoi(str);
   }
   File.close();
   
   std::cout << maxSum << std::endl;
+  std::cout << Sum2 << std::endl;
+  std::cout << Sum3 << std::endl;
+  std::cout << maxSum+Sum2+Sum3 << std::endl;
 }
